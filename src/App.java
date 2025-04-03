38d2f48912783e13d8f4a2eab29b1fc015e925fd
Morg-Owl-141 <nanoassistem@gmail.com>
2025-04-02 18:37:30 -0600

    wegyuwagawy

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Tengo Hambre");
+        System.out.println("Tengo Hambre y Quiero Comer!");
     }
 }
